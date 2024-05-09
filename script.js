5a7b04d8dbe9452470c7363c474cacf9c4e676ec
Shreyash630 <108777853+Shreyash630@users.noreply.github.com>
2024-05-09 15:48:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+const searchButton = document.getElementById('search-button');
+const searchInput = document.getElementById('search-input');
+const mealList = document.getElementById('meal-list');
+let favlist=[];
+// Add event listener to the search button
+searchButton.addEventListener('click', searchMeal);
+
+// Add event listener to the search input to listen for "Enter" key press
+searchInput.addEventListener('keyup', (event) => {
+  if (event.key === 'Enter') {
+    searchMeal();
+  }
+});
+
+// Function to search for meals
+function searchMeal() {
+  const searchValue = searchInput.value.toLowerCase();
+  const meals = mealList.getElementsByTagName('li');
+
+  for (let i = 0; i < meals.length; i++) {
+    const mealName = meals[i].getElementsByTagName('h3')[0].textContent.toLowerCase();
+    if (mealName.includes(searchValue)) {
+      meals[i].style.display = 'block';
+    } else {
+      meals[i].style.display = 'none';
+    }
+  }
+}
+const favButton=document.getElementById('btn-fav');
+favButton.addEventListener('click', addfavMeal);
+ function addfavMeal(){
+alert("Added to favourite");
+ }
+ 
